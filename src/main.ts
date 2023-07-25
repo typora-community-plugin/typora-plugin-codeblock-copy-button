dd051e6b86535460ce63f8d9e140468b07ec9b13
plylrnsdy <plylrnsdy@163.com>
2023-07-26 01:08:53 +0800

    feat: add a copy button to each codeblock's top-right corner.

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import * as _ from "lodash"
+import { CodeblockPostProcessor, I18n, Plugin } from '@typora-community-plugin/core'
+import { File, editor } from "typora"
+
+
+export default class extends Plugin {
+
+  i18n = new I18n({
+    resources: {
+      'en': { buttonCopy: 'Copy' },
+      'zh-cn': { buttonCopy: '复制' },
+    }
+  })
+
+  onload() {
+    this.registerMarkdownPostProcessor(
+      CodeblockPostProcessor.from({
+        button: {
+          text: '<span class="fa fa-clipboard"></span>',
+          title: this.i18n.t.buttonCopy,
+          onclick(event, { code }) {
+            const normalizedCode = code.replace(/\r?\n/g, File.isWin ? '\r\n' : '\n')
+            editor.UserOp.setClipboard(null, null, normalizedCode)
+          },
+        }
+      }))
+  }
+
+  onunload() {
+  }
+}
---
+++ test/vault/doc.md
@@ -0,0 +1,6 @@
+```javascript
+function add(a, b) {
+  return a + b;
+}
+```
+
